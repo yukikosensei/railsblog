4a44e71ec5e6657fbcbef4b0fa17a675b567110c
yukikosensei <wwwdtad@gmail.com>
2021-04-18 21:55:11 -0400

    Changed CSS

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @article.comments.create(comment_params)
-    redirect_to articles_path(@article)
+    redirect_to article_path(@article)
   end
   
   private
+
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :name)
   end
 
 end
